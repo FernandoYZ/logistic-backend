2e3ebda4ca63473b0e8f86e3deb526284a343a13
FernandoYZ <fernandozavala266@gmail.com>
2025-01-31 23:16:24 -0500

    chore(eslint): agregar reglas de Prettier en la configuración de ESLint
    - Se añaden las siguientes reglas de Prettier:
      - `endOfLine: 'auto'`
      - `singleQuote: true`
      - `trailingComma: 'all'`
      - `semi: true`
      - `tabWidth: 2`
      - `printWidth: 100`
    - Estas reglas aseguran un formato consistente en el código base.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,7 +29,15 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': ['error', {
+        endOfLine: 'auto',
+        singleQuote: true,
+        trailingComma: 'all',
+        semi: true,
+        tabWidth: 2,
+        printWidth: 100
+      }]
     },
   },
 );
